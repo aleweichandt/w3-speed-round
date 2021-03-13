12e3c366afd0d3fd2c920a7e6493e723a26a14ed
alew <ale.w.es@sync.money>
2021-03-13 12:55:27 +0100

    login screen

--- app/src/main/java/com/example/androiddevchallenge/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/login/LoginScreen.kt
@@ -15,15 +15,39 @@
  */
 package com.example.androiddevchallenge.ui.screen.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.shapes
 
 @Composable
 fun LoginScreen(onNext: () -> Unit) {
@@ -34,19 +58,106 @@ fun LoginScreen(onNext: () -> Unit) {
 
 @Composable
 fun LoginScreenBody(onNext: () -> Unit) {
-    Surface {
-        Button(
-            onClick = onNext
+    val (email, setEmail) = remember { mutableStateOf("") }
+    val (password, setPassword) = remember { mutableStateOf("") }
+    val bgResource = when (MaterialTheme.colors.isLight) {
+        false -> R.drawable.bg_login_dark
+        else -> R.drawable.bg_login
+    }
+    Image(
+        contentScale = ContentScale.FillBounds,
+        modifier = Modifier.fillMaxSize(),
+        painter = painterResource(id = bgResource),
+        contentDescription = ""
+    )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
+                .padding(16.dp)
         ) {
-            Text(text = stringResource(id = R.string.play))
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 32.dp),
+                text = stringResource(id = R.string.login),
+                style = MaterialTheme.typography.h1,
+                textAlign = TextAlign.Center
+            )
+            TextField(
+                modifier = Modifier
+                    .height(56.dp)
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                value = email,
+                onValueChange = setEmail,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.email),
+                        style = MaterialTheme.typography.body1
+                    )
+                },
+                maxLines = 1,
+                textStyle = MaterialTheme.typography.body1
+            )
+            TextField(
+                modifier = Modifier
+                    .height(56.dp)
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                value = password,
+                onValueChange = setPassword,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.password),
+                        style = MaterialTheme.typography.body1
+                    )
+                },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                maxLines = 1,
+            )
+            Button(
+                modifier = Modifier
+                    .height(72.dp)
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                shape = shapes.medium,
+                onClick = onNext,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.login)
+                )
+            }
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = buildAnnotatedString {
+                    append(stringResource(id = R.string.dont_have_account))
+                    withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
+                        append(stringResource(id = R.string.singup_u))
+                    }
+                },
+                style = MaterialTheme.typography.body1,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
 
-@Preview
+@Preview(widthDp = 360, heightDp = 640)
 @Composable
 fun LoginScreenPreview() {
     MyTheme {
+        LoginScreen(onNext = {})
+    }
+}
+
+@Preview(widthDp = 360, heightDp = 640)
+@Composable
+fun LoginScreenPreviewDark() {
+    MyTheme(darkTheme = true) {
         LoginScreen(onNext = {})
     }
 }
